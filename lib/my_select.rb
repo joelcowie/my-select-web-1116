d572cc1087490397e5a068755ece33c5b052d698
Joel Cowie <joel.a.cowie@gmail.com>
2016-11-23 01:38:29 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  new_array = []
+  collection.each do |number|
+    if number.even?
+      new_array.push(number)
+    end
+  end
+  return new_array
 end
